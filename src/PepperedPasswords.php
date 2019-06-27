12cf34a813abaf27a0e2fce8df70b2f09ee44d53
Jacco-V <jacco.vinke@netsilik.nl>
2019-06-27 09:18:00 +0000

    Add optional cost parameter to constructor

--- src/PepperedPasswords.php
+++ src/PepperedPasswords.php
@@ -25,31 +25,38 @@ final class PepperedPasswords
 	
 	/**
 	 * Constructor
+	 * 
 	 * @param string $pepper The pepper to use as the HMac key
-	 * @return self
+	 * @param int $cost The optional cost factor for the hashing algorithm, default value 12
 	 */
-	public function __construct($pepper)
+	public function __construct(string $pepper, int $cost = 12)
 	{
 		$this->_pepper = $pepper;
+		
+		$this->_cost = $cost;
 	}
 	
 	/**
 	 * Calculate the peppered hash of a password
+	 * 
 	 * @param string $password The password to calculate the hash for
+	 * 
 	 * @return string The peppered hash of the password supplied
 	 */
-	public function hash($password)
+	public function hash(string $password) : string
 	{
-		return password_hash($this->_hmac($password), PASSWORD_DEFAULT);
+		return password_hash($this->_hmac($password), PASSWORD_DEFAULT, ['cost' => $this->_cost]);
 	}
 	
 	/**
 	 * Verify a password against its peppered hash
+	 * 
 	 * @param string $password The password to verify
 	 * @param string $passwordHash The password hash to verify the password against
+	 * 
 	 * @return bool True if the password is correct, false otherwise
 	 */
-	public function verify($password, $passwordHash)
+	public function verify(string $password, string $passwordHash) : bool
 	{
 		return password_verify($this->_hmac($password), $passwordHash);
 	}
@@ -58,7 +65,7 @@ final class PepperedPasswords
 	 * Compute the HMac for the password
 	 * @return string the HMac for the supplied password
 	 */
-	private function _hmac($password) {
+	private function _hmac(string $password) : string {
 		return hash_hmac(self::HMAC_ALGORITHM, $password, $this->_pepper, true);
 	}
 }
